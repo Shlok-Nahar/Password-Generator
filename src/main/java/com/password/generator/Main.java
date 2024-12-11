a5f30c2e63afe181c5d70f868b68fa4f674e65b5
Shlok Nahar <shloknahar925@gmail.com>
2024-12-11 01:39:04 +0100

    Clean-up Code

--- src/main/java/com/password/generator/Main.java
+++ src/main/java/com/password/generator/Main.java
@@ -14,42 +14,41 @@ import java.util.logging.Logger;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Main {
+public class Main 
+{
 
-    private static final Logger logger = Logger.getLogger(Main.class.getName());  // Logger instance
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
     private static final SecureRandom random = new SecureRandom();
     private static final int PASSWORD_LENGTH = 10 + random.nextInt(13);
     private static final String JSON_FILE_PATH = "generated_passwords.json";
 
-    public static void main(String[] args) {
-        setupLogger();  // Set up logger configuration
+    public static void main(String[] args) 
+    {
+        setupLogger();
 
-        try (Scanner scanner = new Scanner(System.in)) {
-            // Log the prompt for the number of passwords
+        try (Scanner scanner = new Scanner(System.in)) 
+        {
             logger.info("Enter the Username or Email to save with the password: ");
             String username = scanner.next();
 
-            // List to store generated passwords
             List<PasswordRecord> passwordList = new ArrayList<>();
 
-            // Generate passwords and add to the list
             String generatedPassword = generatePassword(PASSWORD_LENGTH);
 
-            // Get current date and time
             String currentDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
             String currentTime = new SimpleDateFormat("HH:mm:ss").format(new Date());
 
-            // Log the generated password with appropriate logging level
             logger.info(() -> String.format("Generated Password: %s", generatedPassword));
 
-            // Create PasswordRecord and add to the list
             PasswordRecord passwordRecord = new PasswordRecord(generatedPassword, currentDate, currentTime, username);
             passwordList.add(passwordRecord);
 
-            try {
+            try 
+            {
                 writeToJSON(passwordList);
-            } catch (IOException e) {
+            } catch (IOException e) 
+            {
                 logger.severe("Error while writing passwords to JSON: " + e.getMessage());
             }
 
@@ -57,28 +56,28 @@ public class Main {
         }
     }
 
-    // Set up logger configuration
-    private static void setupLogger() {
-    // Remove all default handlers to avoid duplicate logs
-    Logger rootLogger = Logger.getLogger("");
-    for (var handler : rootLogger.getHandlers()) {
-        rootLogger.removeHandler(handler);
-    }
+    private static void setupLogger() 
+    {
+        Logger rootLogger = Logger.getLogger("");
+        for (var handler : rootLogger.getHandlers()) 
+        {
+            rootLogger.removeHandler(handler);
+        }
 
-    // Add a new console handler
     ConsoleHandler consoleHandler = new ConsoleHandler();
     consoleHandler.setLevel(Level.ALL);
     rootLogger.addHandler(consoleHandler);
     rootLogger.setLevel(Level.ALL);
-}
+    }
 
 
-    // Method to generate a password of a given length
-    public static String generatePassword(int length) {
+    public static String generatePassword(int length) 
+    {
         SecureRandom random = new SecureRandom();
         StringBuilder password = new StringBuilder(length);
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) 
+        {
             int index = random.nextInt(CHARACTERS.length());
             password.append(CHARACTERS.charAt(index));
         }
@@ -86,19 +85,18 @@ public class Main {
         return password.toString();
     }
 
-    // Method to write the list of PasswordRecord to a JSON file
-    public static void writeToJSON(List<PasswordRecord> passwordList) throws IOException {
+    public static void writeToJSON(List<PasswordRecord> passwordList) throws IOException 
+    {
         File file = new File(JSON_FILE_PATH);
         ObjectMapper mapper = new ObjectMapper();
 
-        // If the file exists, append to the existing records
-        if (file.exists()) {
+        if (file.exists()) 
+        {
             List<PasswordRecord> existingRecords = mapper.readValue(file, mapper.getTypeFactory().constructCollectionType(List.class, PasswordRecord.class));
-            existingRecords.addAll(passwordList);  // Add new records to the existing ones
-            mapper.writeValue(file, existingRecords);  // Write the updated list to the file
-        } else {
-            // If the file does not exist, create a new one and write the password records
-            mapper.writeValue(file, passwordList);
-        }
+            existingRecords.addAll(passwordList);
+            mapper.writeValue(file, existingRecords);
+        } 
+        
+        else { mapper.writeValue(file, passwordList); }
     }
 }
--- src/main/java/com/password/generator/PasswordRecord.java
+++ src/main/java/com/password/generator/PasswordRecord.java
@@ -3,60 +3,69 @@ package com.password.generator;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class PasswordRecord {
+public class PasswordRecord 
+{
 
     private String password;
     private String generatedDate;
     private String generatedTime;
-    private String identification;  // New field for identification
+    private String identification;
 
-    // Default constructor for Jackson
-    public PasswordRecord() {
-        this.identification = "";  // Set default empty value
+    public PasswordRecord() 
+    {
+        this.identification = "";
     }
 
-    // Constructor for creating the object with required fields
     @JsonCreator
     public PasswordRecord(@JsonProperty("password") String password,
                           @JsonProperty("generatedDate") String generatedDate,
                           @JsonProperty("generatedTime") String generatedTime,
-                          @JsonProperty("identification") String identification) {
+                          @JsonProperty("identification") String identification) 
+                          {
         this.password = password;
         this.generatedDate = generatedDate;
         this.generatedTime = generatedTime;
-        this.identification = identification != null ? identification : ""; // Ensure it's not null
+        this.identification = identification != null ? identification : "";
     }
 
-    // Getters and Setters for each field
-    public String getPassword() {
+    public String getPassword() 
+    {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password) 
+    {
         this.password = password;
     }
 
-    public String getGeneratedDate() {
+    public String getGeneratedDate() 
+    {
         return generatedDate;
     }
 
-    public void setGeneratedDate(String generatedDate) {
+    public void setGeneratedDate(String generatedDate) 
+    {
         this.generatedDate = generatedDate;
     }
 
-    public String getGeneratedTime() {
+    public String getGeneratedTime() 
+    {
+
         return generatedTime;
     }
 
-    public void setGeneratedTime(String generatedTime) {
+    public void setGeneratedTime(String generatedTime) 
+    {
         this.generatedTime = generatedTime;
     }
 
-    public String getIdentification() {
+    public String getIdentification() 
+    {
         return identification;
     }
 
-    public void setIdentification(String identification) {
+    public void setIdentification(String identification) 
+    {
         this.identification = identification;
     }
 }
